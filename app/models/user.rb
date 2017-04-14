db0f8f4709c46b992e4b7f38fcc78b4748edf8ff
Pekka <dashagera@gmail.com>
2017-04-14 19:32:08 +0300

    Added validators to User and Event, added user reference to events

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+
+  has_secure_password
+
+  has_many :events
+
+  validates :email, :password, presence: true
+
+  validates :email, uniqueness: true, 
+    email_format: { message: "Email is incorrect" }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170414144739) do
+ActiveRecord::Schema.define(version: 20170414161348) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20170414144739) do
     t.string "repeat"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
