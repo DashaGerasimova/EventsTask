92073731366d962c3db026876017c9835b358dd4
Pekka <dashagera@gmail.com>
2017-04-14 20:59:11 +0300

    Added possibility to change languages

--- Gemfile
+++ Gemfile
@@ -32,6 +32,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'bcrypt', '~> 3.1.7'
 # email validator
 gem 'validates_email_format_of'
+#support for additional languages
+ gem 'rails-i18n'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  resources :events
-  get 'sessions/new'
 
-  get 'sessions/create'
 
-  get 'sessions/destroy'
+root to: 'events#index'
+resources :events
+get 'sessions/new'
+
+get 'sessions/create'
+
+get 'sessions/destroy'
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
