10d6c3d45bb9145a97e7af8959c92c759fbaed88
Pekka <dashagera@gmail.com>
2017-04-20 17:49:02 +0300

    Made little code improvments, added profile page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,20 @@
 class SessionsController < ApplicationController
   skip_before_action :ensure_login, only: [:new, :create]
 
-  def new
-  end
-
   def create
     user = User.find_by(email: params[:user][:email])
     password = params[:user][:password]
 
     if user&&user.authenticate(password)
       session[:user_id] = user.id
-      redirect_to events_path, notice: "Logged in successfully"
+      redirect_to user_path(user.id), notice: t('Logged in')
     else 
-      redirect_to login_path, alert: "Invalid login/password"
+      redirect_to login_path, alert: t('Invalid log/pas')
     end
   end
 
   def destroy
     reset_session
-    redirect_to 'login', notice: 'logged out'
+    redirect_to 'login', notice: t('Logged out')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,15 @@
 Rails.application.routes.draw do
 
+  root to: 'events#index'
 
-root to: 'events#index'
+  resources :sessions, only: [:new, :destroy, :create]
 
-resources :sessions, only: [:new, :destroy, :create]
+  resources :events
 
-resources :events
+  resources :users, only: [:new, :create, :edit, :show, :update]
+  get "/profile" => "users#show"
 
-resources :users, only: [:new, :create, :edit, :show, :update]
+  get "/login" => "sessions#new", as: "login"
+  delete "/logout" => "sessions#destroy", as: "logout"
 
-#get "users#new"
-
-get "/login" => "sessions#new", as: "login"
-delete "/logout" => "sessions#destroy", as: "logout"
-
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
