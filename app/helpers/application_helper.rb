82b1ec12a8bb9311e3a0d8dec4c705cd466347eb
Pekka <dashagera@gmail.com>
2017-05-18 22:38:37 +0300

    Added translations for column 'repeat'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  class CustomTranslation
+  	class << self
+  	  def repeat (repeat_value)
+  	  	case repeat_value
+  	  	when 0
+  	  	  result = 'every_day'
+  	  	when 1
+  	  	  result = 'every_week'
+  	  	when 2
+  	  	  result = 'every_month'
+  	  	when 3
+  	  	  result = 'every_year'
+  	  	end 
+  	  	return result
+  	  end
+  	end
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,5 @@ class Event < ApplicationRecord
   belongs_to :user
 
   validates :title, :event_date, presence: true
-  validates :repeat, inclusion: { in: 0...4 }
+  validates :repeat, inclusion: { in: 0..4 }
 end
