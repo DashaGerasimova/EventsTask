3f2635916c6e814af1a76df01009ee56e4043c00
Pekka <dashagera@gmail.com>
2017-09-08 22:33:19 +0300

    Fixed routes and users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,7 @@ Rails.application.routes.draw do
   root to: 'events#index'
 
   resources :events
-
-  resources :users, only: [:new, :create, :edit, :show, :update]
-  get "/signup" => "users#new", as: "signup"
-  get "/profile" => "users#show", as: "profile"
-
-  get "/login" => "sessions#new", as: "login"
-  delete "/logout" => "sessions#destroy", as: "logout"
+  
+  get "/profile" => "users#profile", as: "profile"
 
 end
